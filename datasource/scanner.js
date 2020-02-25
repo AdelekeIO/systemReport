70080ccafa98443e76baf0549f228100a3c46eaf
aio <tom.shegzy@gmail.com>
2020-02-25 16:47:13 +0100

    new dataset

--- datasource/scanner.js
+++ datasource/scanner.js
@@ -1,9 +1,8 @@
 const deviceIds = require("./deviceIds");
+// let deviceIds = ["417790"];
 const axios = require("axios");
 const moment = require("moment");
-const { convertArrayToCSV } = require("convert-array-to-csv");
 const ObjectsToCsv = require("objects-to-csv");
-const converter = require("convert-array-to-csv");
 let summary = [];
 let counter = 0;
 let sow = moment()
@@ -12,33 +11,30 @@ let sow = moment()
   .subtract(1, "days")
   .format("YYYY-MM-DD");
 dates = [];
-for (let index = 0; index < 9; index++) {
+for (let index = 0; index < 12; index++) {
   let newDate = moment(sow)
     .add(index, "days")
     .format("YYYY-MM-DD");
   dates.push(newDate);
 }
-action({ deviceIds, counter }, result => {
+action({ deviceIds, counter }, async result => {
   summary.push(result);
   console.log(summary.length);
-
-  new ObjectsToCsv(summary).toDisk("./report.csv");
-  // const csvFromArrayOfArrays = convertArrayToCSV(summary);
+  await new ObjectsToCsv(summary).toDisk("./report.csv").then(() => {
+    if (summary.length === dates.length) {
+      console.log(++counter + " --- done");
+    }
+  });
 });
 
-async function action({ deviceIds, counter }, cb) {
+async function action({ deviceIds }, cb) {
   let report = [];
   const url =
     "https://e6wa7gkf0i.execute-api.us-west-2.amazonaws.com/dev/devicedata";
-
   deviceIds.map(id => {
-    // if (counter === 11) {
-    //   return report;
-    // }
     dates.map(date => {
       let params = {
         TableName: "Parameters",
-
         KeyConditionExpression:
           "DeviceID = :key and  begins_with(DeviceTimeStamp, :up)",
         ExpressionAttributeValues: {
@@ -56,10 +52,8 @@ async function action({ deviceIds, counter }, cb) {
             cb({ id, date, Count, error: "" });
           } else {
             console.log("In Last Eval Key");
-
             let fc = parseInt(Count);
             params["ExclusiveStartKey"] = LastEvaluatedKey;
-            // let tempQuery = ;
             params["FilterExpression"] = params["KeyConditionExpression"];
             axios
               .post(url + "/scan", params)
@@ -74,16 +68,11 @@ async function action({ deviceIds, counter }, cb) {
                 console.log(err);
               });
           }
-
-          // console.log({ id, date, Count, LastEvaluatedKey });
         })
         .catch(err => {
           console.log(`======${id}==${date}======`);
           cb({ id, date, Count: "", error: JSON.stringify(err) });
         });
     });
-
-    counter++;
   });
-  // cb(report);
 }
